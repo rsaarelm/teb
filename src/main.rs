1feaab90630900822f8591d1782f982df86435e1
Risto Saarelma <risto.saarelma@iki.fi>
2026-06-28 19:52:00 +0300

    Bring over the old table code from ont

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> anyhow::Result<()> {
+    // Read from stdin and write to stdout.
+    let input = io::read_to_string(io::stdin())?;
+    // TODO: CLI options for parse numbers, wipe outputs.
+    let mut table = teb::Table::new(&input, true)?;
+    table.eval(false)?;
+    print!("{table}");
+    Ok(())
 }
