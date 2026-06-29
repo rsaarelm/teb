7e7c71eb5c06165c55eab779cbe1662b26d640d8
Risto Saarelma <risto.saarelma@iki.fi>
2026-06-29 14:25:44 +0300

    Comnand-line interface

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,55 @@
-use std::io;
+use std::{fs, io, path::PathBuf};
+
+use anyhow::bail;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Unformatted input table source, stdin by default
+    #[arg()]
+    input: Option<PathBuf>,
+
+    /// Formatted output table destination, stdout by default
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+
+    /// Clear all spreadsheet output cells.
+    #[arg(long)]
+    clear_outputs: bool,
+
+    /// Treat all cells as text instead of numbers.
+    #[arg(long)]
+    no_number_parsing: bool,
+
+    /// Convert input file in-place, input must be a file path not stdin, and
+    /// output will be ignored.
+    #[arg(short, long)]
+    in_place: bool,
+}
 
 fn main() -> anyhow::Result<()> {
-    // Read from stdin and write to stdout.
-    let input = io::read_to_string(io::stdin())?;
-    // TODO: CLI options for parse numbers, wipe outputs.
-    let mut table = teb::Table::new(&input, true)?;
-    table.eval(false)?;
-    print!("{table}");
+    let cli = Cli::parse();
+
+    let input = match &cli.input {
+        Some(path) => fs::read_to_string(path)?,
+        None => io::read_to_string(io::stdin())?,
+    };
+
+    let mut table = teb::Table::new(&input, !cli.no_number_parsing)?;
+    table.eval(cli.clear_outputs)?;
+
+    if cli.in_place {
+        if let Some(path) = &cli.input {
+            fs::write(path, table.to_string())?;
+        } else {
+            bail!("In-place conversion requires an input file path, not stdin.");
+        }
+    } else if let Some(path) = &cli.output {
+        fs::write(path, table.to_string())?;
+    } else {
+        print!("{table}");
+    }
+
     Ok(())
 }
