9dfae2d5dd81981aeb309b23b091a5b8fbe2f18a
Risto Saarelma <risto.saarelma@iki.fi>
2026-07-01 14:24:42 +0300

    More array wrangling

--- src/array.rs
+++ src/array.rs
@@ -4,63 +4,26 @@ use std::{
 };
 
 pub struct Array {
-    shape: Vec<usize>,
+    shape: Shape,
     data: Vec<f64>,
 }
 
-// Use slice::chunks(n) to iterate cell chunks
-
-pub fn cell_coords(frame_shape: &[usize]) -> impl Iterator<Item = Vec<usize>> + '_ {
-    let mut indices = vec![0; frame_shape.len()];
-    let mut end = false;
-    iter::from_fn(move || {
-        if end {
-            return None;
-        }
-        let current = indices.clone();
-
-        // With empty frame_shape, return a single empty vector then exit.
-        if frame_shape.is_empty() {
-            end = true;
-            return Some(current);
-        }
-
-        for i in 0..frame_shape.len() {
-            indices[i] += 1;
-            if indices[i] < frame_shape[i] {
-                break;
-            } else {
-                indices[i] = 0;
-                if i == frame_shape.len() - 1 {
-                    end = true;
-                    break;
-                }
-            }
-        }
-        Some(current)
-    })
-}
-
 impl Array {
-    pub fn new(shape: Vec<usize>, data: impl IntoIterator<Item = f64>) -> Self {
-        let len = shape.iter().product::<usize>();
+    pub fn new(shape: impl Into<Shape>, data: impl IntoIterator<Item = f64>) -> Self {
+        let shape = shape.into();
+
+        let len = shape.num_cells();
         let data: Vec<f64> = data.into_iter().take(len).collect();
         assert_eq!(data.len(), len, "Insufficient data for array");
         Array { shape, data }
     }
 
-    pub fn shape(&self) -> &[usize] {
+    pub fn shape(&self) -> &Shape {
         &self.shape
     }
 
     pub fn rank(&self) -> usize {
-        self.shape.len()
-    }
-
-    /// Strip high dimensions of size 1, the array is structurally identical
-    /// to an array with these dimensions removed.
-    pub fn effective_rank(&self) -> usize {
-        self.rank() - self.shape.iter().rev().take_while(|&&dim| dim == 1).count()
+        self.shape.rank()
     }
 
     pub fn cell_len(&self, cell_rank: usize) -> usize {
@@ -84,7 +47,10 @@ impl Array {
         // TODO: Abstract pairs method to handle higher-rank operands.
 
         // Array shapes must share a prefix
-        let shared_rank = self.effective_rank().min(other.effective_rank());
+        let shared_rank = self
+            .shape()
+            .effective_rank()
+            .min(other.shape().effective_rank());
         assert_eq!(
             &self.shape[..shared_rank],
             &other.shape[..shared_rank],
@@ -117,65 +83,76 @@ impl DerefMut for Array {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use pretty_assertions::assert_eq;
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
+pub struct Shape {
+    dims: Vec<usize>,
+}
 
-    #[test]
-    fn test_cell_scan() {
-        // Scalar
-        let scalar = Array::new(vec![], vec![42.0]);
+impl Shape {
+    pub fn new(dims: Vec<usize>) -> Self {
+        assert!(
+            dims.iter().all(|&dim| dim > 0),
+            "Array dimensions must be greater than zero"
+        );
+        Shape { dims }
+    }
 
-        assert_eq!(scalar.rank(), 0);
-        assert_eq!(
-            scalar.cells(0).collect::<Vec<_>>(),
-            vec![(vec![], &[42.0][..])]
-        );
+    pub fn rank(&self) -> usize {
+        self.len()
+    }
 
-        // Vector
-        let vector = Array::new(vec![3], vec![1.0, 2.0, 3.0]);
-        assert_eq!(vector.rank(), 1);
-        assert_eq!(
-            vector.cells(0).collect::<Vec<_>>(),
-            vec![
-                (vec![0], &[1.0][..]),
-                (vec![1], &[2.0][..]),
-                (vec![2], &[3.0][..])
-            ]
-        );
-        assert_eq!(
-            vector.cells(1).collect::<Vec<_>>(),
-            vec![(vec![], &[1.0, 2.0, 3.0][..])]
-        );
+    pub fn num_cells(&self) -> usize {
+        self.dims.iter().product()
+    }
 
-        // Matrix
-        let matrix = Array::new(vec![2, 3], vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
-        assert_eq!(matrix.rank(), 2);
-        assert_eq!(
-            matrix.cells(0).collect::<Vec<_>>(),
-            vec![
-                (vec![0, 0], &[1.0][..]),
-                (vec![1, 0], &[2.0][..]),
-                (vec![0, 1], &[3.0][..]),
-                (vec![1, 1], &[4.0][..]),
-                (vec![0, 2], &[5.0][..]),
-                (vec![1, 2], &[6.0][..])
-            ]
-        );
+    /// Strip high dimensions of size 1, the array is structurally identical
+    /// to an array with these dimensions removed.
+    pub fn effective_rank(&self) -> usize {
+        self.rank() - self.dims.iter().rev().take_while(|&&dim| dim == 1).count()
+    }
 
-        assert_eq!(
-            matrix.cells(1).collect::<Vec<_>>(),
-            vec![
-                (vec![0], &[1.0, 2.0][..]),
-                (vec![1], &[3.0, 4.0][..]),
-                (vec![2], &[5.0, 6.0][..])
-            ]
-        );
+    pub fn coords(&self) -> impl Iterator<Item = Vec<usize>> + '_ {
+        let mut indices = vec![0; self.len()];
+        let mut end = false;
+        iter::from_fn(move || {
+            if end {
+                return None;
+            }
+            let current = indices.clone();
 
-        assert_eq!(
-            matrix.cells(2).collect::<Vec<_>>(),
-            vec![(vec![], &[1.0, 2.0, 3.0, 4.0, 5.0, 6.0][..])]
-        );
+            // With empty frame_shape, return a single empty vector then exit.
+            if self.is_empty() {
+                end = true;
+                return Some(current);
+            }
+
+            for i in 0..self.len() {
+                indices[i] += 1;
+                if indices[i] < self[i] {
+                    break;
+                } else {
+                    indices[i] = 0;
+                    if i == self.len() - 1 {
+                        end = true;
+                        break;
+                    }
+                }
+            }
+            Some(current)
+        })
+    }
+}
+
+impl From<Vec<usize>> for Shape {
+    fn from(dims: Vec<usize>) -> Self {
+        Shape::new(dims)
+    }
+}
+
+impl Deref for Shape {
+    type Target = [usize];
+
+    fn deref(&self) -> &Self::Target {
+        &self.dims
     }
 }
